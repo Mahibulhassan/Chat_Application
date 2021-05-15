a9ae69f429ccb1bac01288833dee732d59aae338
Mahibulhassan <mahibulhassan842@gmail.com>
2021-05-15 23:45:32 +0600

    Base design

---
+++ Chat_Application/app/src/main/java/com/mahibul/chat_application/Ui/Home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mahibul.chat_application.Ui.Home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahibul.chat_application.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
